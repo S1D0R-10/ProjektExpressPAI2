dd023888937ee6f35a7bfc236bcd75101c27ff96
Marcel Geba <u35_margeb_lbn@technischools.com>
2025-04-11 21:52:56 +0200

    used controllers

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,23 @@ import express from "express";
 import { connection } from "./config/database.config";
 import { ENV } from "./config/env";
 
-connection() // CONNECT MONGOOSE DB
+import { tableController } from "./resources/tables/table.controller";
+import { reservationController } from "./resources/reservations/reservation.controller";
+import { menuController } from "./resources/menu/menu.controller";
+import { orderController } from "./resources/orders/order.controller";
+
+connection(); // CONNECT MONGOOSE DB
 
 const app = express();
 const PORT = ENV.PORT;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use("/tables", tableController);
+app.use("/menu", menuController);
+app.use("/reservations", reservationController);
+app.use("/orders", orderController);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
-})
-
+    console.log(`Server is listening on port ${PORT}`);
+});
